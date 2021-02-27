cdfd6d6e90b0a616276f806d5632a39fefc8d691
Meron Ogbai <okbaymeron@gmail.com>
2021-02-27 18:32:29 +0300

    Add book validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,10 @@
 class Book < ApplicationRecord
   belongs_to :user
+
+  validates :user, presence: true
+  validates :title, presence: true,
+                    length: { minimum: 2, maximum: 50 }
+  validates :total_chapters, presence: true,
+                             numbericality: { only_integer: true, greater_than: 0, less_than: 1000 }
+  validates :completed_chapters, numbericality: { only_integer: true, less_than: 1000 }
 end
