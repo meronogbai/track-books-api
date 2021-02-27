6708147f17733328cf668eaabd806a7768b534a8
Meron Ogbai <okbaymeron@gmail.com>
2021-02-27 19:11:18 +0300

    Seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(username: 'test_user', password: '123456')
+user.books.create(title: 'Comedy Book', total_chapters: 50, completed_chapters: 2)
+user.books.create(title: 'Sad Book', total_chapters: 100, completed_chapters: 25)
