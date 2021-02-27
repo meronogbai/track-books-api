9a5ca414dea298748b85ec8f784b7454597f7a14
Meron Ogbai <okbaymeron@gmail.com>
2021-02-27 18:01:22 +0300

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :books
+
+  validates :username, presence: true, length: { minimum: 2, maximum: 20 }
+  validates :password, presence: true, length: { minimum: 6 }
 end
