8f57823c06395a2f39d28b6dfbf5c95d5f9b0f36
Meron Ogbai <okbaymeron@gmail.com>
2021-02-28 05:26:46 +0300

    Add books controller, routes and actions

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,33 @@
+class BooksController < ApplicationController
+  before_action :authenticate
+
+  def index
+    books = current_user.books
+    render json: { books: books }
+  end
+
+  def create
+    book = current_user.books.build(book_params)
+    if book.valid?
+      book.save
+      render json: { book: book }
+    else
+      render json: { error: book.errors.full_messages }, status: :not_acceptable
+    end
+  end
+
+  def show
+    book = current_user.books.find_by(id: params[:id])
+    if book
+      render json: { book: book, success: "#{book.title} found." }
+    else
+      render json: { failure: "Book doesn't exist." }, status: :not_found
+    end
+  end
+
+  private
+
+  def book_params
+    params.permit(:title, :total_chapters, :completed_chapters)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     scope :v1 do
       resources :users, only: [:create]
       resources :sessions, only: [:create]
+      resources :books, only: [:index, :create, :show]
     end
   end
 end
