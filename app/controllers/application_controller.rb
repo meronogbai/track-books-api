5da76ed1f8a7c734921afddbb65e48874dc3bb1c
Meron Ogbai <okbaymeron@gmail.com>
2021-02-28 07:18:21 +0300

    Refactor errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,19 +10,19 @@ class ApplicationController < ActionController::API
   end
 
   def decoded_token
+    begin
     auth = request.headers['Authorization']
     if auth
       token = auth.split(' ')[1]
-      begin
         JWT.decode(token, secret, true, algorithm: 'HS256')
-      rescue JWT::DecodeError
-        []
+      rescue
+        nil
       end
     end
   end
 
   def current_user
-    unless decoded_token.empty?
+    if decoded_token
       user_id = decoded_token[0]['user_id']
       User.find_by(id: user_id)
     end
