55ae266b85a29b877aa0c0b07667546d69d925c4
Meron Ogbai <okbaymeron@gmail.com>
2021-03-03 20:52:59 +0300

    Add book update route and action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,6 +25,20 @@ class BooksController < ApplicationController
     end
   end
 
+  def update
+    book = current_user.books.find_by(id: params[:id])
+    if book
+      if params[:completed_chapters].to_i <= book.total_chapters
+        book.update(completed_chapters: params[:completed_chapters])
+        render json: { book: book, success: "#{book.title} updated." }
+      else
+        render json: { failure: "Completed chapters can't be higher than total chapters." }, status: :not_acceptable
+      end
+    else
+      render json: { failure: "Book doesn't exist." }, status: :not_found
+    end
+  end
+
   private
 
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     scope :v1 do
       resources :users, only: [:create]
       resources :sessions, only: [:create]
-      resources :books, only: [:index, :create, :show]
+      resources :books, only: [:index, :create, :show, :update]
     end
   end
 end
