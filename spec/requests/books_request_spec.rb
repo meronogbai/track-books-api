3b9a05929cdb569e5e742e9f7bcaf69bd12914db
Meron Ogbai <okbaymeron@gmail.com>
2021-04-04 07:25:57 +0300

    Add specs for books destroy and update actions

--- spec/requests/books_request_spec.rb
+++ spec/requests/books_request_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Books', type: :request do
   before(:example) do
     @user = User.create(username: 'test_user', password: '123456')
+    @book = @user.books.create(title: 'Test Title', total_chapters: 12, completed_chapters: 10)
     token = JWT.encode({ user_id: @user.id }, Rails.application.secret_key_base)
     @headers = { Authorization: "Bearer #{token}" }
   end
@@ -20,8 +21,17 @@ RSpec.describe 'Books', type: :request do
   end
 
   it 'shows book info' do
-    book = @user.books.create(title: 'Test Title', total_chapters: 12, completed_chapters: 10)
-    get "/api/v1/books/#{book.id}", headers: @headers
+    get "/api/v1/books/#{@book.id}", headers: @headers
+    expect(response).to have_http_status(:ok)
+  end
+
+  it "updates a book's completed chapters" do
+    patch "/api/v1/books/#{@book.id}", params: {completed_chapters: 11}, headers: @headers
+    expect(response).to have_http_status(:ok)
+  end
+
+  it 'deletes a book' do
+    delete "/api/v1/books/#{@book.id}", headers: @headers
     expect(response).to have_http_status(:ok)
   end
 end
